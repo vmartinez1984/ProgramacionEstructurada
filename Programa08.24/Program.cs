fa96498e028f5ac0ce408d6a05a0afe6cad3b7d1
victor bravo <ahal_tocob@hotmail.com>
2019-10-17 23:32:04 -0500

    Tarea 8 completada

---
+++ Programa08.24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Programa08._24
+{
+    class Program
+    {
+        /// <summary>
+        /// 24.  Que calcule la suma de los cuadrados de los 100 primeros números.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula  la suma de los cuadrados de los primeros 100 números");
+
+            int i;
+            int suma =0;
+
+            for (i = 1; i <= 100; i++)
+            {
+                suma = suma + (i * i);
+            }
+
+            Console.WriteLine($"El resultado es: {suma}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programa08.25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa08._25
+{
+    /// <summary>
+    /// 25.  Que escriba los primeros 25 dígitos de la sucesión de Fibonacci.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que escribe los primeros 25 números de la sicesión Fibonacci");
+            int x, y, z, cont;
+
+            x = 0;
+            y = 1;
+
+            for (cont = 1; cont <= 25; cont = cont + 1)
+            {
+                z = x + y;
+                Console.WriteLine(z);
+                x = y;
+                y = z;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
